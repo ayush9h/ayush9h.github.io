3428ae4634a8314877a5700964ad8b094c16bddb
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2023-05-21 10:06:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -61,22 +61,7 @@ $(document).ready(function () {
   });
 });
 
-/**********************************************
------------Tabbing Scripts for JS--------------
- ************************************************/
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
 
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
 
 /**********************************************
 ------------JS FOR CONTACT FORMS--------------
